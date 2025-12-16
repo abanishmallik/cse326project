cb395604ecfbfbdbdc28d3eb7ef81315300dd9bd
Abanish Mallik <abanishmallik2005@gmail.comdd>
2025-12-16 12:39:41 +0530

    first commit

---
+++ food-backend/src/main/java/com/example/foodbackend/FoodBackendApplication.java
@@ -0,0 +1,11 @@
+package com.example.foodbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FoodBackendApplication.class, args);
+    }
+}
